caabda67f1094ea7dcad24124419c8bfd10c8abb
Stijn Robben <98906222+stijn-robben@users.noreply.github.com>
2025-06-19 10:07:29 +0200

    Made webapi and made models

---
+++ Dock-ShipmentCompany/Models/ShipmentCompany.cs
@@ -0,0 +1,17 @@
+﻿namespace Dock_ShipmentCompany.Models
+{
+    public class ShipmentCompany
+    {
+        public int Id { get; set; }
+        public required string CompanyName { get; set; }
+        public required string AddressLine { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+        public required string CardNumber { get; set; }
+        public required string CompanyPhone { get; set; }
+        public required string CompanyEmail { get; set; }
+
+
+        public ICollection<Dock> Docks { get; set; }
+    }
+}
