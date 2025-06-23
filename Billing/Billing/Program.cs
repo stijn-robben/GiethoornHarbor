c667c2422ddce9f3d12723f5e0fca20bc254ae57
Steen010 <kjh.vansteen@student.avans.nl>
2025-06-23 14:53:13 +0200

    Redid mapping

--- Billing/Billing/Program.cs
+++ Billing/Billing/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
